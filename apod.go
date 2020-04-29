6baa9f5f443a8b9cfa544c74cf88a7e03e5ebb70
Dale Campbell <oshuma@gmail.com>
2020-04-29 13:45:04 -0400

    Fix golint warnings.

--- apod.go
+++ apod.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 )
 
-const APOD_URL = "https://api.nasa.gov/planetary/apod"
+const apodAPIURL = "https://api.nasa.gov/planetary/apod"
 
 type APODImage struct {
 	Date           Date   `json:"date"`
 	Title          string `json:"title"`
-	Url            string `json:"url"`
-	HDUrl          string `json:"hdurl"`
+	URL            string `json:"url"`
+	HDURL          string `json:"hdurl"`
 	Explanation    string `json:"explanation"`
 	MediaType      string `json:"media_type"`
 	Copyright      string `json:"copyright"`
@@ -20,7 +20,7 @@ type APODImage struct {
 }
 
 func APOD(p ParamEncoder) (APODImage, error) {
-	req, err := http.NewRequest("GET", APOD_URL, nil)
+	req, err := http.NewRequest("GET", apodAPIURL, nil)
 	if err != nil {
 		return APODImage{}, err
 	}
